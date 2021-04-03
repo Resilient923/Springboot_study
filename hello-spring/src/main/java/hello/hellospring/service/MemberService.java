00b358262ef9a3fc86fbffcebecfe25866c1a2f2
Resilient923 <kc960923@naver.com>
2021-04-03 17:18:32 +0900

    [update] 210403

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,22 +4,40 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.domain.Member;
 
+import java.util.List;
 import java.util.Optional;
 
-
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     //회원가입
-    public Long join(Member member){
+    public Long join(Member member) {
 
-        //중복이름회원은 안된다.
-
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m-> {
-            throw new IllegalStateException("이미존재하는 회원입니다.");
-        });
+        //중복이름회원검증
+        validateDuplicateMember(member);
+        //위와같은경우에는 method로 뽑는게 좋다.
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()) //findByName을 하고 그 결과는 optional멤버
+                .ifPresent(m -> {//만약에 존재하면
+                    throw new IllegalStateException("이미존재하는 회원입니다.");
+                });
+    }
+    
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 }
