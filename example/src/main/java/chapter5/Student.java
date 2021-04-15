fe8c754e568738f6ba7f7eefe875466cd4e19209
Resilient923 <kc960923@naver.com>
2021-04-15 13:34:16 +0900

    [update] 210415

--- example/src/main/java/chapter5/Student.java
+++ example/src/main/java/chapter5/Student.java
@@ -6,7 +6,17 @@ public class Student { // 클래스 속성은 프로퍼티
     int grade;
     String address;
 
-    public void showStudnetInfo(){
+    //클래스에 생성자가 하나도 없으면 자바 컴파일러가 생성자를 자체적으로 만들어 둔다.
+     public Student(int id, String name){
+        studentID = id;
+        studentName = name;
+     } //-> default 생성자 : 매개변수, 구현코드가 없다
+
+    //위에 방식을 메소드 오버로딩 생성자 오버로딩이라 한다.
+    //생성자는 class이름과 동일
+
+
+    public void showStudentInfo(){
 
         System.out.println(studentName + "," + address);
     }
@@ -16,5 +26,7 @@ public class Student { // 클래스 속성은 프로퍼티
     public void setStudentName(String name){ // void 반환값
         studentName = name;
     }
+
+
 
 }
--- example/src/main/java/chapter5/StudentTest.java
+++ example/src/main/java/chapter5/StudentTest.java
@@ -2,10 +2,27 @@ package chapter5;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student studentLee = new Student();
-        studentLee.studentName = "스테디";
+
+
+        int i =0; //프리미티브 타입 4byte 정수
+        Student studentLee = new Student(10,"이순신"); //생성자 -> 힙메모리에 생성
+
         studentLee.address = "서울시 마포구";
 
-        studentLee.showStudnetInfo();
+
+        Student studentKim = new Student(400,"tlsrbjf"); //생성자 -> 힙메모리에 생성 new에 의해 생성 alloc
+        //heap은 가비지 컬렉터에서 없애준다.  heap메모리에 이름,id,주소,grade 메서드등 멤버변수넣어서 생성
+        //new를 쓰면 인스턴스 생성
+        //new를 써서만든 생성된 인스턴스를 객체라고한다.
+        studentKim.studentName = "SSS";
+        studentKim.studentID = 50;
+        studentKim.address = "서울시 마포구";
+
+        studentLee.showStudentInfo();
+        studentKim.showStudentInfo();
+
+        //studentLee : 참조변수
+        System.out.println(studentLee);//16진수 주소값출력 (참조값)reference value
+        System.out.println(studentKim);//16진수 주소값출력
     }
 }
