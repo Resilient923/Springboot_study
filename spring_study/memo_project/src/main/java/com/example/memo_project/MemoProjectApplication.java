c4df3648df70f5c291434a5f826a38332fa68ec1
Resilient923 <kc960923@naver.com>
2021-07-09 01:28:03 +0900

    [update] memoController 210708

--- spring_study/memo_project/src/main/java/com/example/memo_project/MemoProjectApplication.java
+++ spring_study/memo_project/src/main/java/com/example/memo_project/MemoProjectApplication.java
@@ -2,11 +2,14 @@ package com.example.memo_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MemoProjectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MemoProjectApplication.class, args);
     }
 
--- spring_study/memo_project/src/main/java/com/example/memo_project/controller/MemoController.java
+++ spring_study/memo_project/src/main/java/com/example/memo_project/controller/MemoController.java
@@ -1,9 +1,14 @@
 package com.example.memo_project.controller;
 
+import com.example.memo_project.domain.Memo;
 import com.example.memo_project.domain.MemoRepository;
+import com.example.memo_project.domain.MemoRequestDto;
 import com.example.memo_project.service.MemoService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,4 +16,27 @@ public class MemoController {
 
     private final MemoRepository memoRepository;
     private final MemoService memoService;
+
+    @PostMapping("/api/memos")
+    public Memo createMemo(@RequestBody MemoRequestDto requestDto){
+        Memo memo = new Memo(requestDto);
+        return memoRepository.save(memo);
+    }
+
+    @GetMapping("/api/memos")
+    public List<Memo> readMemo(){
+        return memoRepository.findAllByOrderByModifiedAtDesc();
+    }
+
+    @DeleteMapping("/api/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id){
+        memoRepository.deleteById(id);
+        return id;
+    }
+
+    @PutMapping("/api/memos/{id}")
+    public Long update(@PathVariable Long id, @RequestBody MemoRequestDto requestDto){
+        memoService.update(id, requestDto);
+        return id;
+    }
 }
