311f9b48e9e3a84e34ab24b0f9acff3a4dd1ca62
Resilient923 <kc960923@naver.com>
2021-07-10 00:01:39 +0900

    [add] selectshopProject_firstCommit 210709

---
+++ spring_study/selectshopProject/src/main/java/com/example/selectshopproject/SelectshopProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.selectshopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectshopProjectApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SelectshopProjectApplication.class, args);
+    }
+
+}
---
+++ spring_study/selectshopProject/src/test/java/com/example/selectshopproject/SelectshopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selectshopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectshopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
